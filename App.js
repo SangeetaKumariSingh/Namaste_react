72c148e2c8ba6cef7646f5bef4c841af377b1af5
Sangeeta Kumari Singh <sangeetasingh95224@gmail.com>
2026-07-08 22:06:39 +0530

    Lesson 3 coding assignment

--- App.js
+++ App.js
@@ -1,53 +1,64 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import download from "url:./src/assets/images/download.jpg";
+import logo from "url:./src/assets/images/logo.png";
+// import userIcon from "./src/assets/userIcon.png"; // Create React Root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//! Ques Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class “title”) and same with jsx
+// React Element using React.createElement()
+const heading = React.createElement("div", { className: "mainheading" }, [
+  React.createElement("h1", { key: "1" }, "Heading One"),
+  React.createElement("h2", { key: "2" }, "Heading Two"),
+  React.createElement("h3", { key: "3" }, "Heading Three"),
+]);
 
-/*
- * React Functional Component
- * - A JavaScript function that returns JSX  or a react element.
- * - Component names must start with a CAPITAL letter.
- */
-const Heading = () => {
-  return <h1>Hello World from Function 1</h1>;
-};
+// root.render(heading);
 
-/*
- * Functional component using the implicit return syntax.
- * No 'return' keyword is needed because there are no {} braces.
- */
-const Heading2 = () => <h1>Hello World from Function 2</h1>;
-
-/*
- * Functional component with JSX attributes.
- */
-const Heading3 = () => <h1 id="heading3">Hello World from Function 3</h1>;
-
-/*
- * Component Composition
- *Calling a component inside another component is Component Composition.
- * Here, Heading4 is the parent component.
- * Heading, Heading2, and Heading3 are child components.
- */
-const Heading4 = () => {
+// Functional Component using JSX
+const JsxHeading = () => {
   return (
-    <>
-      {/* React Fragment (<>...</>)
-          Groups multiple elements without creating an extra HTML element. */}
-
-      <Heading />
-      <Heading2 />
-      <Heading3 />
-    </>
+    <div className="divMain">
+      <h1>Heading One from Functional Component using JSX</h1>
+      <h2>Heading Two from Functional Component using JSX</h2>
+      <h3>Heading Three from Functional Component using JSX</h3>
+    </div>
   );
 };
 
-/*
- * Create a React Root.
- * This connects React with the HTML root element.
- */
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// JSX React Element
+const JsxHeading2 = (
+  <div className="divMain">
+    <h1>Heading One - JsxHeading2</h1>
+    <h2>Heading Two - JsxHeading2</h2>
+    <h3>Heading Three - JsxHeading2</h3>
+    {/* component Composition */}
+    <JsxHeading />
+  </div>
+);
 
-/*
- * Render the root React component.
- * React starts rendering from Heading4,
- * which in turn renders Heading, Heading2, and Heading3.
- */
-root.render(<Heading4 />);
+// Render JSX React Element
+// root.render(JsxHeading2);
+console.log("image", logo);
+console.log("Hello React");
+
+// ! Ques Create a Header Component from scratch using Functional Components with JSX, Add a Logo,Add a search bar in middle,Add User icon on right.
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoIcon">
+        <p align="left">
+          <img src={download} width="100px" height="100px" alt="Logo" />
+        </p>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+      </div>
+      <div className="user-icon">
+        <img src={logo} height="100px" alt="User Icon" />
+      </div>
+    </div>
+  );
+};
+// Render Header Component
+root.render(<Header />);
