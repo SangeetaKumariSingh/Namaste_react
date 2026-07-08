0cabd3bb3294caca126be8b07cab730bae702316
Sangeeta Kumari Singh <sangeetasingh95224@gmail.com>
2026-07-08 09:47:59 +0530

    Lesson 3 Laying the Foundation

--- App.js
+++ App.js
@@ -1,3 +1,53 @@
+import ReactDOM from "react-dom/client";
+
+/*
+ * React Functional Component
+ * - A JavaScript function that returns JSX  or a react element.
+ * - Component names must start with a CAPITAL letter.
+ */
+const Heading = () => {
+  return <h1>Hello World from Function 1</h1>;
+};
+
+/*
+ * Functional component using the implicit return syntax.
+ * No 'return' keyword is needed because there are no {} braces.
+ */
+const Heading2 = () => <h1>Hello World from Function 2</h1>;
+
+/*
+ * Functional component with JSX attributes.
+ */
+const Heading3 = () => <h1 id="heading3">Hello World from Function 3</h1>;
+
+/*
+ * Component Composition
+ *Calling a component inside another component is Component Composition.
+ * Here, Heading4 is the parent component.
+ * Heading, Heading2, and Heading3 are child components.
+ */
+const Heading4 = () => {
+  return (
+    <>
+      {/* React Fragment (<>...</>)
+          Groups multiple elements without creating an extra HTML element. */}
+
+      <Heading />
+      <Heading2 />
+      <Heading3 />
+    </>
+  );
+};
+
+/*
+ * Create a React Root.
+ * This connects React with the HTML root element.
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("div", {},"Hello world from React");
-root.render(parent);
+
+/*
+ * Render the root React component.
+ * React starts rendering from Heading4,
+ * which in turn renders Heading, Heading2, and Heading3.
+ */
+root.render(<Heading4 />);
